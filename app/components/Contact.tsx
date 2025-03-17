1d88cfcf02383a790db06e11bcdd2348a38dd877
citizenz7 <citizenz7@protonmail.com>
2025-03-17 16:35:59 +0100

    fix contact form

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -21,14 +21,14 @@ export const Contact = () => {
 
         <div className="bg-white/10 p-6 rounded-xl max-w-[1000px]">
             <h2 className="text-center text-xl md:text-2xl font-bold text-orange-400 mb-4">Contactez-moi à l&#39;aide du formulaire ci-dessous</h2>
-            <form className="space-y-4" action="https://getform.io/f/ajjmyrda" method="POST">
+            <form className="relative space-y-4" action="https://getform.io/f/ajjmyrda" method="POST">
                 <div className="grid md:grid-cols-2 gap-4">
-                    <input type="text" name="firstname" placeholder="Prénom" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400" />
-                    <input type="text" name="name" placeholder="Nom" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400" />
-                    <input type="email" name="email" placeholder="E-mail" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400" />
-                    <input type="text" name="phone" placeholder="Téléphone" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400" />
+                    <input type="text" name="firstname" placeholder="Prénom" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full" />
+                    <input type="text" name="name" placeholder="Nom" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full" />
+                    <input type="email" name="email" placeholder="E-mail" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full" />
+                    <input type="text" name="phone" placeholder="Téléphone" className="bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full" />
                 </div>
-                <textarea name="message" rows={7} placeholder="Votre message" className="bg-black/70 w-full rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400" />
+                <textarea name="message" rows={7} placeholder="Votre message" className="bg-black/70 w-full rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400 w-full" />
                 <button type="submit" className="bg-orange-700 hover:bg-orange-500 text-white px-6 py-2 w-full font-semibold text-xl rounded-xl">Envoyer</button>
             </form>
         </div>
